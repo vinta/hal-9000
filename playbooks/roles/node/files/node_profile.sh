d6ca69ec9cc2f2dcf7609b542126a26e2159eb49
Vinta Chen <vinta.chen@gmail.com>
2019-10-22 03:40:36 +0800

    use nvm

--- playbooks/roles/node/files/node_profile.sh
+++ playbooks/roles/node/files/node_profile.sh
@@ -1,1 +1,3 @@
-export PATH="/usr/local/opt/node@10/bin:$PATH"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
