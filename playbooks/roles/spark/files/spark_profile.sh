ee6e7c095ed7fff19aae792c45faebcfb10a2752
Vinta Chen <vinta.chen@gmail.com>
2020-06-23 15:54:12 +0800

    install apach spark 3.0.0 and java 11

--- playbooks/roles/spark/files/spark_profile.sh
+++ playbooks/roles/spark/files/spark_profile.sh
@@ -1,10 +1,8 @@
 if which java > /dev/null; then
-  export JAVA_HOME="$(/usr/libexec/java_home -v 1.8)"
+  export JAVA_HOME="$(/usr/libexec/java_home -v11)"
   export PATH="$JAVA_HOME/bin:$PATH"
 fi
 
-export PATH="/usr/local/opt/scala@2.12/bin:$PATH"
-
-export SPARK_HOME="/usr/local/share/apache-spark/spark-2.4.3"
+export SPARK_HOME="/usr/local/share/apache-spark/spark-3.0.0"
 export PATH="$SPARK_HOME/bin:$PATH"
 export PYTHONPATH="$SPARK_HOME/python:$PYTHONPATH"
