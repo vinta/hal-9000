cef7dabc7c54733b2f897a82b5fc803102b6dac2
Vinta Chen <vinta.chen@gmail.com>
2022-12-19 03:11:36 +0800

    refactor

--- playbooks/roles/kubernetes/files/k8s_profile.sh
+++ playbooks/roles/kubernetes/files/k8s_profile.sh
@@ -1,6 +1,6 @@
-if [ -f "$HOME/vendors/google-cloud-sdk/path.zsh.inc" ]; then
-  source "$HOME/vendors/google-cloud-sdk/path.zsh.inc"
-  source "$HOME/vendors/google-cloud-sdk/completion.zsh.inc"
+if [ -f "$HOME/.google-cloud-sdk/path.zsh.inc" ]; then
+  source "$HOME/.google-cloud-sdk/path.zsh.inc"
+  source "$HOME/.google-cloud-sdk/completion.zsh.inc"
 fi
 
 if [ $commands[kubectl] ]; then
@@ -9,6 +9,6 @@ fi
 
 [ -f "$HOME/.fubectl.source" ] && source "$HOME/.fubectl.source"
 
-# autoload -U colors; colors
-# source ~/vendors/zsh-kubectl-prompt/kubectl.zsh
-# RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
+autoload -U colors; colors
+source ~/.zsh-kubectl-prompt/kubectl.zsh
+RPROMPT='%{$fg[blue]%}($ZSH_KUBECTL_PROMPT)%{$reset_color%}'
--- playbooks/roles/spark/files/spark_profile.sh
+++
@@ -1,8 +0,0 @@
-if which java > /dev/null; then
-  export JAVA_HOME="$(/usr/libexec/java_home -v11)"
-  export PATH="$JAVA_HOME/bin:$PATH"
-fi
-
-export SPARK_HOME="/usr/local/share/apache-spark/spark-3.0.0"
-export PATH="$SPARK_HOME/bin:$PATH"
-export PYTHONPATH="$SPARK_HOME/python:$PYTHONPATH"
