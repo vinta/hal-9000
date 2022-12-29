336271ea38eef3e67dded921a19999d9888004b2
Vinta Chen <vinta.chen@gmail.com>
2022-12-29 16:19:09 +0800

    use the new plugin for GKE authentication

--- playbooks/roles/kubernetes/files/k8s_profile.sh
+++ playbooks/roles/kubernetes/files/k8s_profile.sh
@@ -1,4 +1,5 @@
 if [ -f "$HOME/.google-cloud-sdk/path.zsh.inc" ]; then
+  export USE_GKE_GCLOUD_AUTH_PLUGIN=True
   source "$HOME/.google-cloud-sdk/path.zsh.inc"
   source "$HOME/.google-cloud-sdk/completion.zsh.inc"
 fi
