68113a8a464a664b0fe9dc0c40f3542b21cabf5b
Vinta Chen <vinta.chen@gmail.com>
2023-04-13 15:00:06 +0800

    add poetry path

--- playbooks/roles/python/files/pyenv_profile.sh
+++ playbooks/roles/python/files/pyenv_profile.sh
@@ -3,3 +3,6 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
+
+# https://python-poetry.org/docs/#installation
+export PATH="/Users/vinta/.local/bin:$PATH"
