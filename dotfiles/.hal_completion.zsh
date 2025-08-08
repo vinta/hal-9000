9054746f1491931b934c63dfd801eefcaeff94cb
Vinta Chen <vinta.chen@gmail.com>
2025-08-09 02:49:13 +0800

    config: update zsh completion script for new copy command
    - Replace 'unlink' command with 'copy' in completion script
    - Update command descriptions to match new functionality

--- dotfiles/.hal_completion.zsh
+++ dotfiles/.hal_completion.zsh
@@ -7,7 +7,7 @@ _hal() {
     commands=(
         'update:pull the repo and run ansible-playbook'
         'link:add the file to the dotfiles repository'
-        'unlink:remove the file from the dotfiles repository'
+        'copy:copy the file to the dotfiles repository'
         'sync:force sync dotfiles'
         'open-the-pod-bay-doors:open the pod bay doors, please, HAL'
     )
@@ -21,7 +21,7 @@ _hal() {
     else
         # Additional arguments based on command
         case "$words[2]" in
-            link|unlink)
+            link|copy)
                 _files
                 ;;
             *)
